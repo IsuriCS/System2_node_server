a6ad42c1d13987d7b82000ce42ba7998a5ecf9d7
Isuri Chathurika <isurics2@gmail.com>
2024-11-18 09:19:13 +0530

    Add server.js file, rename and move authorization.js file to authorization folder

--- functions/authorization/authorization.js
+++ functions/authorization/authorization.js
@@ -2,20 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
-
 
 app.use(bodyParser.json());
-
 
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-  
-    const validUsername = 'testUser';
-    const validPassword = 'abc123';
-
-    if (username === validUsername && password === validPassword) {
+    if (username === 'testUser' && password === 'abc123') {
         return res.status(200).json({ message: 'Login successful!' });
     } else {
         return res.status(401).json({ error: 'Invalid username or password' });
@@ -23,6 +16,5 @@ app.post('/login', (req, res) => {
 });
 
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+
+module.exports = app;
